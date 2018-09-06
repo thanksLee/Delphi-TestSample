0a77dbee6c8b13bd6aaaa5f9bb43c279fc581738
thanksLee <dmzone75@naver.com>
2018-09-06 15:00:30 +0900

    구글번역

---
+++ googletranslate_sample/Unit1.dfm
@@ -0,0 +1,94 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Form1'
+  ClientHeight = 602
+  ClientWidth = 961
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object WebBrowser1: TWebBrowser
+    Left = 24
+    Top = 24
+    Width = 929
+    Height = 257
+    TabOrder = 0
+    ControlData = {
+      4C00000004600000901A00000000000000000000000000000000000000000000
+      000000004C000000000000000000000001000000E0D057007335CF11AE690800
+      2B2E12620A000000000000004C0000000114020000000000C000000000000046
+      8000000000000000000000000000000000000000000000000000000000000000
+      00000000000000000100000000000000000000000000000000000000}
+  end
+  object Button1: TButton
+    Left = 24
+    Top = 304
+    Width = 75
+    Height = 25
+    Caption = #48264#50669
+    TabOrder = 1
+    OnClick = Button1Click
+  end
+  object Button2: TButton
+    Left = 120
+    Top = 304
+    Width = 89
+    Height = 25
+    Caption = #44208#44284' '#44032#51256#50724#44592
+    TabOrder = 2
+    OnClick = Button2Click
+  end
+  object RichEdit1: TRichEdit
+    Left = 24
+    Top = 344
+    Width = 409
+    Height = 225
+    Font.Charset = HANGEUL_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -11
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    Lines.Strings = (
+      '<b>bookstore</b>- <i>n. </i>a store that sells books'
+      
+        '<b>card</b> - <i>n.</i> a small piece of stiff paper that is use' +
+        'd for playing games'
+      
+        '<b>day</b> - <i>n.</i> the part of the day when light from the s' +
+        'un can be seen'
+      
+        '<b>driver'#39's license</b>- <i>n.</i> an official document or card ' +
+        'which shows that you have the legal right to drive a vehicle'
+      
+        '<b>else</b> - <i>adv. </i>used to refer to a different or additi' +
+        'onal person or thing'
+      
+        '<b>night</b> - <i>n. </i>the time of darkness between one day an' +
+        'd the next'
+      
+        '<b>perform</b> - <i>v.</i> to entertain an audience by singing o' +
+        'r acting'
+      '<b>poetry</b> - <i>n.</i> the writings of a poet'
+      
+        '<b>recite</b> - <i>v.</i> to read (something) out loud or say (s' +
+        'omething) from memory'
+      
+        '<b>test</b> - <i>n.</i> a set of questions or problems that are ' +
+        'designed to measure a person'#39's knowledge, skills, or abilities'
+      
+        '<b>trick</b> - <i>n.</i> a clever and skillful action that someo' +
+        'ne performs to entertain or amuse people'
+      'Lesson 20: What Can You Do?'
+      'Let'#39's Learn English Lesson 20: What Can You Do?"')
+    ParentFont = False
+    ScrollBars = ssBoth
+    TabOrder = 3
+    Zoom = 100
+  end
+end
---
+++ googletranslate_sample/Unit1.pas
@@ -0,0 +1,127 @@
+﻿unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.OleCtrls, SHDocVw, Vcl.StdCtrls,
+  Vcl.ComCtrls, MSHTML;
+
+type
+  TForm1 = class(TForm)
+    WebBrowser1: TWebBrowser;
+    Button1: TButton;
+    Button2: TButton;
+    RichEdit1: TRichEdit;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+  pv_LoopCnt : Integer;
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  Doc : IHTMLDocument3;
+  lv_Langbtn, lv_Langbtn1 : IHTMLElement;
+  Coll : IHTMLElementCollection;
+  I : Integer;
+  lv_div: IHTMLDivElement;
+  tmpStr : String;
+  tmpLeftBoolean, tmpRightBoolean : Boolean;
+
+  doc2 : IHTMLDocument2;
+begin
+   //http://bloodguy.tistory.com/entry/Delphi-HTML-%ED%8E%98%EC%9D%B4%EC%A7%80%EC%9D%98-%ED%83%9C%EA%B7%B8-%EC%82%AC%EC%9D%B4%EB%A5%BC-%EC%9C%A0%EB%9E%91%ED%95%98%EA%B8%B0
+
+   pv_LoopCnt := 0;
+   WebBrowser1.Navigate('https://translate.google.co.kr/');
+
+   while WebBrowser1.ReadyState <> READYSTATE_COMPLETE do
+   begin
+      Sleep(5);
+      Application.ProcessMessages;
+   end;
+
+//   if Supports(WebBrowser1.Document, IHTMLDocument3, Doc) and
+//        Supports(Doc.getElementById('sugg-item-en'), IHTMLElement, lv_Langbtn) then
+//   begin
+//      lv_Langbtn.click;
+//   end;
+
+   doc2 := WebBrowser1.Document as IHTMLDocument2;
+
+   tmpLeftBoolean := False;
+   tmpRightBoolean := False;
+   for I := 0 to doc2.all.length - 1 do
+   begin
+
+      lv_Langbtn := doc2.all.item(I, 0) as IHTMLElement;
+
+      if (LowerCase(lv_Langbtn.tagName) = 'div') and (lv_Langbtn.id = 'gt-lang-left') then
+      begin
+         tmpLeftBoolean := True;
+         Continue;
+      end;
+      if (tmpLeftBoolean = True) and (lv_Langbtn.id = 'sugg-item-en')  then
+      begin
+         lv_Langbtn.click;
+         tmpLeftBoolean := False;
+      end;
+
+      if (LowerCase(lv_Langbtn.tagName) = 'div') and (lv_Langbtn.id = 'gt-lang-right') then
+      begin
+         tmpRightBoolean := True;
+         Continue;
+      end;
+
+      if (tmpRightBoolean = True) and (lv_Langbtn.id = 'sugg-item-ko')  then
+      begin
+         lv_Langbtn.click;
+         Break;
+      end;
+
+   end;
+
+
+   //WebBrowser1.OleObject.Document.getElementByID('source').Value:='apartment - n. a usually rented room or set of rooms that is part of a building and is used as a place to live. She lives in an apartment.';
+   WebBrowser1.OleObject.Document.getElementByID('source').Value:=RichEdit1.Text;
+   //WebBrowser1.OleObject.Document.getElementByID('source').Value:='平成24年3月1日より、日本国際教育支援協会の日本語能力試験ウェブサイトをリニューアルしました。';
+   Button2Click(Sender);
+
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+var
+   doc : IHTMLDocument3;
+   lv_Langbtn1 : IHTMLElement;
+begin
+   Supports(WebBrowser1.Document, IHTMLDocument3, doc);
+   lv_Langbtn1 := doc.getElementByID('result_box');
+   lv_Langbtn1.removeAttribute('span', 0);
+   RichEdit1.Text := lv_Langbtn1.outerText;
+
+   while lv_Langbtn1.outerText = '' do
+   begin
+      if pv_LoopCnt > 500 then Exit;
+      
+      Sleep(50);
+      Application.ProcessMessages;
+      Inc(pv_LoopCnt);
+      Button2Click(Sender);
+   end;
+
+
+   //RichEdit1.Text := WebBrowser1.OleObject.Document.getElementByID('result_box').innerHtml;
+end;
+
+end.
